b6b73efa4882cdef6c3e43bb2607c44c8d209608
gert <gglukmann@gmail.com>
2015-05-13 11:56:18 +0300

    header update

--- assets/js/visa/_searchbox-responsive.js
+++ assets/js/visa/_searchbox-responsive.js
@@ -11,40 +11,42 @@ $(function(){
     });
 
     /* TYPEAHEAD */
-    var searchResults = new Bloodhound({
-        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-        queryTokenizer: Bloodhound.tokenizers.whitespace,
-        prefetch: 'http://twitter.github.io/typeahead.js/data/films/post_1960.json',
-        remote: {
-            url: 'http://twitter.github.io/typeahead.js/data/films/queries/%QUERY.json',
-            wildcard: '%QUERY'
-        }
-    });
-    searchResults.initialize();
+    // var searchResults = new Bloodhound({
+    //     datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+    //     queryTokenizer: Bloodhound.tokenizers.whitespace,
+    //     prefetch: 'http://twitter.github.io/typeahead.js/data/films/post_1960.json',
+    //     remote: {
+    //         url: 'http://twitter.github.io/typeahead.js/data/films/queries/%QUERY.json',
+    //         wildcard: '%QUERY'
+    //     }
+    // });
+    // searchResults.initialize();
 
-    $('[role="search"] .form-control').typeahead(
-        {
-            hint: true,
-            highlight: true,
-            minLength: 1
-        },
-        {
-            name:'search-results',
-            display:'value',
-            source:searchResults,
-            limit:4,
-            templates: {
-                empty: [
-                    '<div class="empty-message">',
-                    'unable to find any results that match the current query',
-                    '</div>'
-                ].join('\n'),
-                suggestion: function(data){
-                    return '<div><img src="/assets/ico/favicon-32x32.png" alt=""/><span>' + data.value + '</span></div>'
-                },
-                footer:'<div class="tt-footer"><a href="#">Vaata koiki <span class="icon icon-arrow-right"></span></a></div>'
-            }
-        }
-    );
+    // $('[role="search"] .form-control').typeahead(
+    //     {
+    //         hint: true,
+    //         highlight: true,
+    //         minLength: 1
+    //     },
+    //     {
+    //         name:'search-results',
+    //         display:'value',
+    //         source:searchResults,
+    //         limit:4,
+    //         templates: {
+    //             empty: [
+    //                 '<div class="empty-message">',
+    //                 'unable to find any results that match the current query',
+    //                 '</div>'
+    //             ].join('\n'),
+    //             suggestion: function(data){
+    //                 return '<div><img src="/assets/ico/favicon-32x32.png" alt=""/><span>' + data.value + '</span></div>'
+    //             },
+    //             footer:'<div class="tt-footer"><a href="#">Vaata koiki <span class="icon icon-arrow-right"></span></a></div>'
+    //         }
+    //     }
+    // );
+
+    $('[role="search"] .form-control').wrap('<div class="twitter-typeahead"></div>');
 
 });
